21dde7e3e9b6b2e8cc4e6cdcd86612a546015a24
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-12-02 17:22:37 +0530

    get mov con

---
+++ src/controllers/movieController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import Movie from '../models/Movie';
+
+export const addMovie = async (req: Request, res: Response) => {
+  try {
+    const newMovie = new Movie(req.body);
+    const savedMovie = await newMovie.save();
+    res.status(201).json(savedMovie);
+  } catch (error) {
+    res.status(500).json({ message: 'Error adding movie', error });
+  }
+};
+
+export const getAllMovies = async (req: Request, res: Response) => {
+  try {
+    const movies = await Movie.find().sort({ createdAt: -1 }); 
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching movies', error });
+  }
+};
