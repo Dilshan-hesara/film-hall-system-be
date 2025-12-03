d105f332dab9ac251ee543d9cdac1b27fbd1ed89
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-12-03 19:47:34 +0530

    add: create Booking model with schema and interface

---
+++ src/models/Booking.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IBooking extends Document {
+  user: mongoose.Types.ObjectId;
+  movie: mongoose.Types.ObjectId;
+  date: string;
+  time: string;
+  seats: string[];
+  totalPrice: number;
+}
+
+const BookingSchema: Schema = new Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    movie: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie', required: true },
+    date: { type: String, required: true },
+    time: { type: String, required: true },
+    seats: { type: [String], required: true },
+    totalPrice: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<IBooking>('Booking', BookingSchema);
