b44533e7481f065f80ade44c4c1752160b983d75
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-12-06 19:55:48 +0530

    add: implement route to get movie by ID

--- src/routes/hallRoutes.ts
+++ src/routes/hallRoutes.ts
@@ -6,4 +6,10 @@ const router = express.Router();
 router.post('/add', addHall); // POST: http://localhost:5000/api/v1/halls/add
 router.get('/all', getAllHalls);
 
+
+import { getHallById } from '../controllers/hallController';
+
+// ...
+router.get('/:id', getHallById); // GET: /api/v1/halls/65df...
+
 export default router;
--- src/routes/movieRoutes.ts
+++ src/routes/movieRoutes.ts
@@ -7,4 +7,10 @@ router.post('/add', addMovie);
 
 router.get('/all', getAllMovies);
 
+
+import {  getMovieById } from '../controllers/movieController';
+
+// ...
+router.get('/:id', getMovieById);
+
 export default router;
