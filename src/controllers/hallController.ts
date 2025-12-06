57d35d4828f88daa12307bbebfed87a85c2eecec
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-12-06 19:56:14 +0530

    add getHallById function

--- src/controllers/hallController.ts
+++ src/controllers/hallController.ts
@@ -6,13 +6,12 @@ export const addHall = async (req: Request, res: Response) => {
   try {
     const { name, rows, columns } = req.body;
 
-    // නම දැනටමත් තියෙනවද බලනවා
     const existingHall = await Hall.findOne({ name });
     if (existingHall) {
       return res.status(400).json({ message: 'Hall name already exists' });
     }
 
-    // Capacity එක Auto Calculate වෙනවා
+    // Capacity  Auto Calculate 
     const capacity = rows * columns;
 
     const newHall = new Hall({
@@ -30,12 +29,24 @@ export const addHall = async (req: Request, res: Response) => {
   }
 };
 
-// 2. Get All Halls (Dropdowns සඳහා අවශ්‍ය වෙයි)
+// 2. Get All Halls
 export const getAllHalls = async (req: Request, res: Response) => {
   try {
     const halls = await Hall.find();
     res.status(200).json(halls);
   } catch (error) {
     res.status(500).json({ message: 'Error fetching halls', error });
+  }
+};
+
+export const getHallById = async (req: Request, res: Response) => {
+  try {
+    const hall = await Hall.findById(req.params.id);
+    if (!hall) {
+      return res.status(404).json({ message: 'Hall not found' });
+    }
+    res.status(200).json(hall);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching hall', error });
   }
 };
