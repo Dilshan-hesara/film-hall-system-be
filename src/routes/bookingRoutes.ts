357386c98c41f350ead1161d288097f042ea9064
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-12-09 21:25:56 +0530

    booking routes to include user-specific bookings and all bookings retrieval

--- src/routes/bookingRoutes.ts
+++ src/routes/bookingRoutes.ts
@@ -1,9 +1,19 @@
 import express from 'express';
-import { createBooking, getOccupiedSeats } from '../controllers/bookingController';
+import { cancelBooking, createBooking, getOccupiedSeats ,getAllBookings} from '../controllers/bookingController';
 
 const router = express.Router();
 
 router.post('/create', createBooking);
 router.get('/occupied', getOccupiedSeats);
+
+
+import { getBookingsByUser } from '../controllers/bookingController';
+
+// ...
+// GET: http://localhost:5000/api/v1/bookings/user/123
+router.get('/user/:userId', getBookingsByUser);
+
+router.get('/all', getAllBookings);       // GET: /api/v1/bookings/all
+router.delete('/:id', cancelBooking);
 
 export default router;
