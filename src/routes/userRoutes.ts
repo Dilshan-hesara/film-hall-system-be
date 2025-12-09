a24878ffc871346aadd04f65ca3091d2b79da628
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-12-09 21:24:00 +0530

    d admin management routes for creating admins and resetting passwords

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { updateProfile, changePassword ,getAllUsers , updateUserRole ,deleteUser } from '../controllers/userController';
+import { updateProfile, changePassword ,getAllUsers , updateUserRole ,deleteUser, createAdmin, getAllAdmins, adminResetPassword } from '../controllers/userController';
 
 const router = express.Router();
 
@@ -10,5 +10,9 @@ router.put('/password', changePassword); // PUT: /api/v1/users/password
 router.get('/all', getAllUsers);
 router.put('/role/:id', updateUserRole);
 router.delete('/:id', deleteUser);
+
+router.post('/admin/create', createAdmin);
+router.put('/admin/reset-password/:id', adminResetPassword);
+router.get('/admin/all', getAllAdmins);
 
 export default router;
