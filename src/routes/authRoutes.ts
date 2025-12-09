d5f8f67fe9e3c9891e93c6d8c59bc069c06cdb93
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-12-09 21:25:27 +0530

    add authentication routes for OTP verification and password management

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,13 @@
 import express from 'express';
-import { registerUser, loginUser, refreshToken } from '../controllers/authController';
+import { registerUser, loginUser, refreshToken, verifyOTP,forgotPassword, resetPassword } from '../controllers/authController';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/refresh', refreshToken); 
+router.post('/verify-otp', verifyOTP);
+router.post('/forgot-password',forgotPassword);
+router.post('/reset-password',resetPassword)
 
 export default router;
