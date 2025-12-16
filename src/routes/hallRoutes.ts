37e7383c5253e30c945fc39d2c88990680c3b6a7
Dilshan Hesara <hesarsdilshan@gmail.com>
2025-12-16 17:58:54 +0530

    update and delete endpoints

--- src/routes/hallRoutes.ts
+++ src/routes/hallRoutes.ts
@@ -1,15 +1,18 @@
 import express from 'express';
-import { addHall, getAllHalls } from '../controllers/hallController';
+import { addHall, deleteHall, getAllHalls, updateHall } from '../controllers/hallController';
 
 const router = express.Router();
 
-router.post('/add', addHall); // POST: http://localhost:5000/api/v1/halls/add
+router.post('/add', addHall); 
 router.get('/all', getAllHalls);
 
 
 import { getHallById } from '../controllers/hallController';
 
-// ...
-router.get('/:id', getHallById); // GET: /api/v1/halls/65df...
+router.get('/:id', getHallById); 
+
+
+router.put('/:id', updateHall);
+router.delete('/:id', deleteHall);
 
 export default router;
