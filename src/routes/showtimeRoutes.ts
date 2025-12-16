7b5b9fbfbc233b96334a73d82ddd5376e666fd51
Dilshan Hesara <hesarsdilshan@gmail.com>
2025-12-16 18:01:57 +0530

    add deleteShowtime endpoint and improve showtime routes

--- src/routes/showtimeRoutes.ts
+++ src/routes/showtimeRoutes.ts
@@ -1,9 +1,14 @@
 import express from 'express';
-import { addShowtime, getShowtimesByMovie } from '../controllers/showtimeController';
+import { addShowtime, deleteShowtime, getShowtimesByMovie, getShowtimesByDate ,getShowtimesByMovieDate} from '../controllers/showtimeController';
 
 const router = express.Router();
 
-router.post('/add', addShowtime); // POST: /api/v1/showtimes/add
-router.get('/movie/:movieId', getShowtimesByMovie); // GET: /api/v1/showtimes/movie/123
+router.post('/add', addShowtime); 
+router.get('/movie/:movieId', getShowtimesByMovie); 
 
+router.post('/add', addShowtime);
+router.get('/schedule', getShowtimesByDate); 
+router.delete('/:id', deleteShowtime);
+
+router.get('/movie-date', getShowtimesByMovieDate);
 export default router;
