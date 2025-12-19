80510463ea9d3f14cff37acb0a4f613d5c83f1d9
Dilshan Hesara <hesarsdilshan@gmail.com>
2025-12-19 08:50:24 +0530

    update Booking model to allow optional user field and add support for online payment method

--- src/models/Booking.ts
+++ src/models/Booking.ts
@@ -8,17 +8,42 @@ export interface IBooking extends Document {
   time: string;
   seats: string[];
   totalPrice: number;
+  status: 'Booked' | 'Used'| 'Paid' | 'Cancelled'; 
+
+  paymentMethod: 'Card' | 'Cash';
+  guestInfo?: { 
+    name: string;
+    phone: string;
+  };
+
 }
 
 const BookingSchema: Schema = new Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
     movie: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie', required: true },
     hall: { type: mongoose.Schema.Types.ObjectId, ref: 'Hall', required: true },
     date: { type: String, required: true },
     time: { type: String, required: true },
     seats: { type: [String], required: true },
     totalPrice: { type: Number, required: true },
+    status: { 
+      type: String, 
+      enum: ['Booked', 'Used', 'Paid','Cancelled'], 
+      default: 'Booked' 
+    },
+
+paymentMethod: {
+    type: String,
+    required: true,
+    enum: ['Cash', 'Card', 'ONLINE_PAYMENT'] 
+  },
+      guestInfo: {
+      name: { type: String },
+      phone: { type: String },
+    },
+
+    
   },
   { timestamps: true }
 );
